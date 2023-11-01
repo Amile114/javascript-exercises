988ad895385b2a0dec667453a045d97b86dcefdd
Zofir Amile <Zofir.Amile114@gmail.com>
2023-11-01 13:10:42 -0400

    completed exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let splitStr =  str.split('');
+  let reverseStr = splitStr.reverse();
+  let joinStr = reverseStr.join('');
 
+  return joinStr;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// ALTERNATIVE WAY
+// const reverseString = function(str) {
+//   let newString = '';
+
+//   for (let i = str.length -1; i >= 0; i--) {
+//     newString += str[i];
+//     console.log(newString)
+//   }
+      // return newString;
+// }
+
+// console.log(reverseString('howdy'))
